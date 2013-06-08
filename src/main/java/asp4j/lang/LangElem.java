c118e7e0110e616f1cb9e8ed09cdbe7e22c861a7
hbeck <harald.beck@gmx.at>
2013-06-08 16:09:22 +0200

    removed HasArity interface, moved method to LangElem

--- src/main/java/asp4j/lang/LangElem.java
+++ src/main/java/asp4j/lang/LangElem.java
@@ -4,6 +4,8 @@ package asp4j.lang;
  *
  * @author hbeck May 30, 2013
  */
-public interface LangElem extends HasArity, HasSymbol, HasArgs {
+public interface LangElem extends HasSymbol {
+    
+    int arity();
 
 }
